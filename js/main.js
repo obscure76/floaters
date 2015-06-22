4480bfcca26844b01c74f2d19786a56c2ffb2fa8
obscure76 <midhun.nitw@gmail.com>
2015-06-21 23:10:07 -0700

    Readme added

--- js/main.js
+++ js/main.js
@@ -649,10 +649,10 @@ window.requestAnimFrame = (function(callback) {
       }
       
       var canvas = document.getElementById('myCanvas');
-      //canvas.width = window.innerWidth;
-      //canvas.height = window.innerHeight;
-      canvas.width = 500;
-      canvas.height = 400;
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+      //canvas.width = 500;
+      //canvas.height = 400;
       var date = new Date();
       var time = date.getTime();
       var trolls = [];
